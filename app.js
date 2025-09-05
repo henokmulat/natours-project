85ab4b36e2d7dd58ba3ca4da03603c52412150e9
henokmulat <henokmulat27@gmail.com>
2025-09-05 09:03:22 +0300

    creating and mouting multiple routes

--- app.js
+++ app.js
@@ -139,26 +139,20 @@ const deleteUser = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours/:id', getTour);
+//Routes
 
-app.patch('/api/v1/tours/:id', updateTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.delete('/api/v1/tours/:id', deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = 3000;
 
