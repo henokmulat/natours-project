042ff851786cc9d4ab2646530f9038c5a8b4827d
henokmulat <henokmulat27@gmail.com>
2025-10-02 10:28:28 +0300

    add sorting feature to the API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,42 @@
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
-  const tours = await Tour.find();
   try {
+    console.log(req.query);
+
+    //BUILD QUERY
+    // 1 A) FILTERING
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //1 B) ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    //gte, gt, lte,lt
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
