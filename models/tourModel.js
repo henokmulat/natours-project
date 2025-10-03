9126c40767a3b654f3875ac75088cc6bd28e7435
henokmulat <henokmulat27@gmail.com>
2025-10-03 14:07:40 +0300

    use aggregation middleware to hide secret tours in the aggeration endpoints

--- models/tourModel.js
+++ models/tourModel.js
@@ -94,7 +94,13 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`The query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
