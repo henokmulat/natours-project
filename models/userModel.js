124f2c760ef0391643fec123172e7e86c747df3e
henokmulat <henokmulat27@gmail.com>
2025-10-05 22:39:01 +0300

    Manage passwords by hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //name,email.photo,password,passwordConfirm
 
@@ -25,7 +26,23 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // this works only on SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // only run this function if password was modified
+  if (!this.isModified('password')) return next();
+  //Hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete the password confirm field
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model('User', userSchema);
