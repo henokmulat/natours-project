f74a469d44bff52fd99c9c8ee93cfb04d699ab0f
henokmulat <henokmulat27@gmail.com>
2025-10-13 21:50:49 +0300

    add hpp middleware to prevent parameter pollution

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -35,6 +36,8 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 // Data sanitation against XSS
 app.use(xss());
+// Prevent parameter pollution
+app.use(hpp());
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 // Test middleware
