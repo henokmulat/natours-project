770195c10a4de1ca696d490004a1e987fb5bf7a2
henokmulat <henokmulat27@gmail.com>
2025-10-14 16:30:26 +0300

    refactor createReview method to streamline review creation process inside tour route

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -32,12 +32,9 @@ exports.getReview = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
-  // const review = await Review.create({
-  //   review: req.body.review,
-  //   rating: req.body.rating,
-  //   tour: req.body.tour,
-  //   user: req.body.user,
-  // });
+  // Allow nested route
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const review = await Review.create(req.body);
   res.status(201).json({
     status: 'Success',
