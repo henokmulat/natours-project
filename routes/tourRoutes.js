f28cd04d25704b93ad86545634b8e0b1cdfa3285
henokmulat <henokmulat27@gmail.com>
2025-10-14 22:27:21 +0300

    refactor review routes integration in tour routes for improved structure

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
-// router.param('id', tourController.checkId);
+router.use('/tourId/reviews', reviewRouter);
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -26,14 +26,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour,
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
   );
 
 module.exports = router;
