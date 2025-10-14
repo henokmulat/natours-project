2c100bffef3fccee628e556b76760000699e065e
henokmulat <henokmulat27@gmail.com>
2025-10-14 15:06:25 +0300

    add review controller with Create and Read operations for user reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,48 @@
+const Review = require('../models/reviewModel');
+const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const reviews = await features.query;
+
+  res.status(200).json({
+    status: 'Success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+  if (!review) return new AppError('No review found with that Id!', 404);
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      review,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  // const review = await Review.create({
+  //   review: req.body.review,
+  //   rating: req.body.rating,
+  //   tour: req.body.tour,
+  //   user: req.body.user,
+  // });
+  const review = await Review.create(req.body);
+  res.status(201).json({
+    status: 'Success',
+    data: {
+      review,
+    },
+  });
+});
