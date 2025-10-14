40eab9b3a0e8b4a38937440e387f41439e4a0d33
henokmulat <henokmulat27@gmail.com>
2025-10-14 11:34:08 +0300

    populate guides field in tour model queries

--- models/tourModel.js
+++ models/tourModel.js
@@ -156,6 +156,13 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`The query took ${Date.now() - this.start} milliseconds`);
   next();
