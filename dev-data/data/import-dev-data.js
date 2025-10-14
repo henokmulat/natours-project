ea539a44459d9cce2df649943d8891e283058bec
henokmulat <henokmulat27@gmail.com>
2025-10-14 10:22:12 +0300

    update import-dev-data.js to read from tours.json and extend tourModel.js with startLocation and locations fields

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -24,9 +24,7 @@ mongoose
   .catch((err) => console.error('DB Connection Error:', err));
 
 //READ THE FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 //IMPORT DATA INTO DB
 
