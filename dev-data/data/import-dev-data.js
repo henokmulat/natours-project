67c1ec0e8eba72bad0b02942e8c8174d2a8fcf1d
henokmulat <henokmulat27@gmail.com>
2025-10-15 17:35:43 +0300

    enhance data import and deletion functionality for users and reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -25,12 +27,18 @@ mongoose
 
 //READ THE FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
-
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 //IMPORT DATA INTO DB
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+
     console.log('Data loaded successfully!');
   } catch (err) {
     console.log(err);
@@ -42,6 +50,9 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+
     console.log('Data deleted successfully!');
   } catch (err) {
     console.log(err);
