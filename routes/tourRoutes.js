45ebdf826bf960bbb7bfa440cd4bca3495930249
henokmulat <henokmulat27@gmail.com>
2025-10-16 11:52:05 +0300

    add getToursWithin functionality to retrieve tours based on distance and location

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -20,6 +20,9 @@ router
     tourController.getMonthlyPlan,
   );
 router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(
