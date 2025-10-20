dc4741d5efd036f0ac439631dd905a22e17fb0bb
henokmulat <henokmulat27@gmail.com>
2025-10-20 15:55:17 +0300

    add a method to handle change password

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,17 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 // DOM ELements
-const mabox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const mapbox = document.getElementById('map');
+const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 // VALUES
-
 // DELEGATION
-if (mabox) {
+if (mapbox) {
   const locations = JSON.parse(mapbox.dataset.locations);
   displayMap(locations);
 }
@@ -25,8 +27,34 @@ if (loginForm) {
 }
 
 if (logoutBtn) {
-  logoutBtn.addEventListener('click', (e) => {
+  logoutBtn.addEventListener('click', logout);
+}
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    logout();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password ').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password',
+    );
+    document.querySelector('.btn--save-password ').textContent =
+      'Save Password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
