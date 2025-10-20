9b07e5b1cf1272e8d44be6b8e6cb0204a435bc77
henokmulat <henokmulat27@gmail.com>
2025-10-20 16:18:04 +0300

    implement multer file upload middleware for updateMe route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const multer = require('multer');
 
 const router = express.Router();
 
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+
+const upload = multer({ dest: 'public/img/users' });
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
@@ -16,7 +20,7 @@ router.use(authController.protect);
 router.patch('/updateMyPassword', authController.updatePassword);
 
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', upload.single('photo'), userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
